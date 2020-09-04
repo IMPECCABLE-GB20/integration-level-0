dbe4d93ea17b72499732a3265e4291c3a04ddd44
mturilli <matteo.turilli@gmail.com>
2020-09-04 18:27:01 -0400

    Clen up wf3

--- src/radical/gb20-covid19/wf3.py
+++ src/radical/gb20-covid19/wf3.py
@@ -2,35 +2,43 @@ import glob
 from radical.entk import Pipeline, Stage, Task
 
 
-def esmacs(rct_stage, stage, outdir="equilibration", name=None):
+def esmacs(names, stage, outdir="equilibration", name=None):
 
-    for i in range(1, 13):
-        t = Task()
-        t.pre_exec = [
-            "export WDIR=\"{}/{}\"".format(run_dir, name),
-            ". {}".format(conda_init),
-            "conda activate {}".format(esmacs_tenv),
-            "module load {}".format(esmacs_tmodules),
-            "mkdir -p $WDIR/replicas/rep{}/{}".format(i, outdir),
-            "cd $WDIR/replicas/rep{}/{}".format(i, outdir),
-            "rm -f {}.log {}.xml {}.dcd {}.chk".format(stage, stage, stage, stage),
-            "export OMP_NUM_THREADS=1"
-            ]
-        # t.executable = '/ccs/home/litan/miniconda3/envs/wf3/bin/python3.7'
-        t.executable = 'python3'
-        t.arguments = ['$WDIR/{}.py'.format(stage)]
-        t.post_exec = []
-        t.cpu_reqs = {
-            'processes': 1,
-            'process_type': None,
-            'threads_per_process': 4,
-            'thread_type': 'OpenMP'}
-        t.gpu_reqs = {
-            'processes': 1,
-            'process_type': None,
-            'threads_per_process': 1,
-            'thread_type': 'CUDA'}
-        rct_stage.add_tasks(t)
+    s = Stage()
+
+    for comp in names:
+        for i in range(1, 13):
+            t = Task()
+
+            t.pre_exec = [
+                "export WDIR=\"{}/{}\"".format(run_dir, name),
+                ". {}".format(conda_init),
+                "conda activate {}".format(esmacs_tenv),
+                "module load {}".format(esmacs_tmodules),
+                "mkdir -p $WDIR/replicas/rep{}/{}".format(i, outdir),
+                "cd $WDIR/replicas/rep{}/{}".format(i, outdir),
+                "rm -f {}.log {}.xml {}.dcd {}.chk".format(stage, stage, stage, stage),
+                "export OMP_NUM_THREADS=1"]
+
+            # t.executable = '/ccs/home/litan/miniconda3/envs/wf3/bin/python3.7'
+            t.executable = 'python3'
+            t.arguments = ['$WDIR/{}.py'.format(stage)]
+            t.post_exec = []
+
+            t.cpu_reqs = {
+                'processes': 1,
+                'process_type': None,
+                'threads_per_process': 4,
+                'thread_type': 'OpenMP'}
+
+            t.gpu_reqs = {
+                'processes': 1,
+                'process_type': None,
+                'threads_per_process': 1,
+                'thread_type': 'CUDA'}
+            s.add_tasks(t)
+
+    return s
 
 
 def wf3(cfg):
@@ -47,19 +55,13 @@ def wf3(cfg):
     p = Pipeline()
     p.name = 'ESMACS'
 
-    s1 = Stage()
-    for comp in esmacs_names:
-        esmacs(rct_stage=s1, stage="eq1", name=comp)
+    s1 = esmacs(esmacs_names, stage="eq1", name=comp)
     p.add_stages(s1)
 
-    s2 = Stage()
-    for comp in esmacs_names:
-        esmacs(rct_stage=s2, stage="eq2", name=comp)
+    s2 = esmacs(esmacs_names, stage="eq2", name=comp)
     p.add_stages(s2)
 
-    s3 = Stage()
-    for comp in esmacs_names:
-        esmacs(rct_stage=s3, stage="sim1", outdir="simulation", name=comp)
+    s3 = esmacs(esmacs_names, stage="sim1", outdir="simulation", name=comp)
     p.add_stages(s3)
 
     return p
