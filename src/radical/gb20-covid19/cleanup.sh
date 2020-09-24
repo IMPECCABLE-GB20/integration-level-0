f24108df7119fa5e99fad31da19b41af54f5f40e
mturilli <matteo.turilli@gmail.com>
2020-09-24 14:04:41 -0400

    Must cleanup before running

---
+++ src/radical/gb20-covid19/cleanup.sh
@@ -0,0 +1,7 @@
+rm -rf $WF2_BASE_PATH/MD_exps/*/omm_runs_*
+rm -rf $WF2_BASE_PATH/MD_to_CVAE/cvae_input.h5
+rm -rf $WF2_BASE_PATH/CVAE_exps/cvae_runs_*
+rm -rf $WF2_BASE_PATH/Outlier_search/outlier_pdbs/*
+rm -rf $WF2_BASE_PATH/Outlier_search/eps_record.json
+rm -rf $WF2_BASE_PATH/Outlier_search/restart_points.json
+rm -rf $WF2_BASE_PATH/CVAE_exps/*runs*
