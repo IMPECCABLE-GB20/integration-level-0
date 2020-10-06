14598aad87417def5fd703dba9d2a47b7f4e1e77
mturilli <matteo.turilli@gmail.com>
2020-10-06 16:02:31 -0400

    Iteration ml1

--- src/radical/gb20-covid19/ml1.py
+++ src/radical/gb20-covid19/ml1.py
@@ -14,46 +14,48 @@ def generate_ml1_pipeline(cfg):
 
     # Native RCT
     # ML task
-    t.pre_exec = [
-        '. /sw/summit/python/3.6/anaconda3/5.3.0/etc/profile.d/conda.sh',
-        'module load gcc/7.4.0',
-        'module load python/3.6.6-anaconda3-5.3.0',
-        'module load cuda/10.1.243',
-        'module load hdf5/1.10.4',
-        'export LC_ALL=en_US.utf-8',
-        'export LANG=en_US.utf-8',
-        #'source activate %s' % cfg['ml1_conda_env'],
-        'conda activate %s' % cfg['ml1_conda_env'],
-        'export device=${OMPI_COMM_WORLD_LOCAL_RANK:=0}',
-        'export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:%s' % cfg['ld_lib_path'],
-        # 'export data_root=%s' % cfg['data_root'],
-        'cd %s' % cfg['base_path']
-        ]
-    t.executable = ['%s/python' % cfg['python_bin_path']]
-    t.arguments = [
-        '-u'  , 'infer_images.py',
-        '-t'  , '0',
-        '-d'  , '${device}',
-        '-i'  , '"%s/images_compressed/*.pkl.gz"' % cfg['data_root'],
-        '-o'  , cfg['output_dir'],
-        '-m'  , cfg['model'],
-        '-trt', cfg['trt'],
-        '--stage_dir', '/mnt/bb/%s' % cfg['userid'],
-        '--num_stage_workers', '4',
-        '--output_frequency', '200',
-        '--update_frequency', '10',
-        '-b', '256',
-        '-j', '4',
-        '-dtype=fp16',
-        '-num_calibration_batches=10',
-        '--distributed'
-        ]
+    # CUDA_STUFF_DIR = "%s/cuda-%s/targets/ppc64le-linux" % (cfg['env_dir'], cfg['cuda_ver'])
+
+    # t.pre_exec = [
+    #     'module load gcc/7.4.0',
+    #     'module load python/3.6.6-anaconda3-5.3.0',
+    #     'module load hdf5/1.10.4',
+    #     'module load cuda/11.0.3',
+    #     'source activate %s' % cfg['ml1_conda_env'],
+    #     'export PATH=%s/bin:${PATH}' % CUDA_STUFF_DIR,
+    #     'export LD_LIBRARY_PATH=%s/lib:%s/lib:${LD_LIBRARY_PATH}' % (cfg['rdbase'], CUDA_STUFF_DIR),
+    #     'export PYTHONPATH=%s:${PYTHONPATH}' % cfg['rdbase'],
+    #     'export LC_ALL=en_US.utf-8',
+    #     'export LANG=en_US.utf-8',
+    #     'export device=${OMPI_COMM_WORLD_LOCAL_RANK:=0}',
+    #     'cd %s' % cfg['base_path']
+    #     ]
+    # t.executable = ['%s/python' % cfg['python_bin_path']]
+    # t.arguments = [
+    #     '-u'  , 'infer_images.py',
+    #     '-t'  , '0',
+    #     '-d'  , '${device}',
+    #     '-i'  , '"%s/images_compressed/*.pkl.gz"' % cfg['data_root'],
+    #     '-o'  , cfg['output_dir'],
+    #     '-m'  , cfg['model'],
+    #     '-trt', cfg['trt'],
+    #     '--stage_dir', '/mnt/bb/%s' % cfg['userid'],
+    #     '--num_stage_workers', '4',
+    #     '--output_frequency', '200',
+    #     '--update_frequency', '10',
+    #     '-b', '256',
+    #     '-j', '4',
+    #     '-dtype=fp16',
+    #     '-num_calibration_batches=10',
+    #     '--distributed'
+    #     ]
 
     # Bash wrapper
-    # t.executable = ['%s/ml1.sh' % cfg['exec_path']]
-    # t.arguments = ['--distributed', cfg['ld_lib_path'], cfg['data_root'],
-    #                cfg['base_path'], cfg['ml1_conda_env'], cfg['output_dir'],
-    #                cfg['model'], cfg['trt'], cfg['userid']]
+    t.executable = ['%s/ml1.sh' % cfg['exec_path']]
+    t.arguments = ['--distributed', cfg['ld_lib_path'], cfg['data_root'],
+                   cfg['base_path'], cfg['ml1_conda_env'], cfg['output_dir'],
+                   cfg['model'], cfg['trt'], cfg['userid'], cfg['env_dir'],
+                   cfg['rdbase']]
 
     t.cpu_reqs = {
         'processes'          : cfg['processes'],
--- src/radical/gb20-covid19/ml1.sh
+++ src/radical/gb20-covid19/ml1.sh
@@ -9,22 +9,36 @@
 # $7 = cfg['model']
 # $8 = cfg['trt']
 # $9 = cfg['userid']
+# $10 = cfg['env_dir']
+# $11 = cfg['rdbase']
 
+PYTHON_VER=3.6
+CUDA_VER=11.0
 
-. /sw/summit/lmod/lmod/init/profile
 module load gcc/7.4.0
 module load python/3.6.6-anaconda3-5.3.0
-module load cuda/10.1.243
 module load hdf5/1.10.4
+module load cuda/11.0.3
+
+source activate ${5}
+
+# add to path
+export ENV_DIR=${10}
+export CUDA_STUFF_DIR=${ENV_DIR}/cuda-${CUDA_VER//.}/targets/ppc64le-linux
+export PATH=${CUDA_STUFF_DIR}/bin:${PATH}
+export LD_LIBRARY_PATH=${CUDA_STUFF_DIR}/lib:${LD_LIBRARY_PATH}
+# rdkit
+export RDBASE=${11}
+export LD_LIBRARY_PATH=${RDBASE}/lib:${LD_LIBRARY_PATH}
+export PYTHONPATH=${RDBASE}:${PYTHONPATH}
 
 export LC_ALL=en_US.utf-8
 export LANG=en_US.utf-8
 export device=${OMPI_COMM_WORLD_LOCAL_RANK:=0}
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${2}       # %s' % cfg['ld_lib_path'],
+# export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${2}       # %s' % cfg['ld_lib_path'],
 export data_root=${3}                                # %s' % cfg['data_root'],
 
 cd ${4}                                              # %s' % cfg['base_path']
-source activate ${5}
 
 # unimproved run
 python3 -u infer_images.py \
