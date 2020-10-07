d6a7e7994756e08bb3db815d23b97789b61bd789
mturilli <matteo.turilli@gmail.com>
2020-10-07 01:01:03 -0400

    Skeleton for RP S3

---
+++ src/radical/gb20-covid19/wf3_multidvm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# $1 = comp
+# $2 = i
+# $3 = outdir
+# $4 = stage
+# $5 = cfg['conda_init']
+# $6 = cfg['conda_esmacs_task_env']
+# $7 = cfg['esmacs_task_modules']
+
+. /sw/summit/lmod/lmod/init/profile
+export WDIR="${1}" # ".format(comp),
+
+. $5
+conda activate $6
+module load $7
+
+mkdir -p ${WDIR}/replicas/rep${2}/${3}  # .format(i, outdir),
+cd ${WDIR}/replicas/rep${2}/${3}        # ".format(i, outdir),
+
+# rm -f ${4}.log ${4}.xml ${4}.dcd ${4}.chk # ".format(stage, stage, stage, stage),
+export OMP_NUM_THREADS=1
+
+python3 ${WDIR}/eq1.py    # '.format(stage)]
+python3 ${WDIR}/sim1.py
